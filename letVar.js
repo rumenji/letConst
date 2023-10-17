82455c0a036c21b6f138646413dffcd5482ee4b0
rumenji <rumen_ji@yahoo.com>
2023-10-17 11:38:02 -0400

    completed

---
+++ letVar.js
@@ -0,0 +1,14 @@
+// ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+// ES2015 Global Constants
+const PI = 3.14;
+
+//var can be redeclared and reassigned, let cannot be redeclared
+
+//var can be redeclared and reassigned, const cannot be redeclared or reassigned
+
+//let can be reassigned, const cannot
+
+//hositing is when using var, it is declared first at the top of the file and then assigned
+//further down in the code.
